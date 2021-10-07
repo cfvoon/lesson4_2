2bb7672c5f1f9aff8512905ba9af130a333f1a88
cfvoon <92003960+cfvoon@users.noreply.github.com>
2021-10-07 09:09:16 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -8,7 +8,18 @@ def hello_world():
     if request_type_str == 'GET':
         return render_template('index.html', href2='')
     else:
-        num_A = request.form['A']
-        num_B = request.form['B']
-        final_result = int(num_A) + int(num_B)
-        return render_template('index.html', href2='A = '+str(num_A)+', B= '+str(num_B)+', A + B = '+str(final_result))
+        value_bread = 2
+        value_cake = 3
+        value_coffee = 2
+        value_tea = 1
+        num_bread = request.form['bread']
+        num_cake = request.form['cake']
+        num_coffee = request.form['coffee']
+        num_tea = request.form['tea']
+        total_bread = int(num_bread)*value_bread
+        total_cake = int(num_cake)*value_cake
+        total_coffee = int(num_coffee)*value_coffee
+        total_tea = int(num_tea)*value_tea
+        
+        final_result = total_bread + total_cake + total_coffee + total_tea
+        return render_template('index.html', href2='Bread = '+str(num_bread)+', Cake= '+str(num_cake)+', Coffee='+str(num_coffee)+', Tea='+str(num_tea)+',  Total = '+str(final_result))
